e0fce306de1c98c97e34579ee795b1607f16d3aa
ashish yadav <aay110597@gmail.com>
2024-12-04 20:04:52 +0530

    First upload

---
+++ prisma/migrations/20241204113712_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `roleId` INTEGER NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Role` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `role` VARCHAR(191) NOT NULL,
+    `desc` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `Role_role_key`(`role`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `Role`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ src/users/users.service.ts
@@ -0,0 +1,19 @@
+
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User, Prisma } from '@prisma/client';
+
+
+@Injectable()
+export class UsersService {
+
+    constructor(private prisma: PrismaService) {}
+    async user(
+        userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+      ): Promise<User | null> {
+        return this.prisma.user.findUnique({
+          where: userWhereUniqueInput,
+        });
+      }
+
+}
